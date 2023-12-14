965bc9a60ce00fdd670d1697cd10074ff96273f5
45akshat <98400519+45akshat@users.noreply.github.com>
2023-12-15 00:24:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -373,35 +373,11 @@ AddPageVisibility()
 
 
 function createHtmlFromJson() {
-    var exportHtml = ""
+    var exportHtml = `<!DOCTYPE html><html> <head> <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""> <link href="https://fonts.googleapis.com/css2?family=Alegreya&amp;family=Poppins:ital,wght@0,500;1,600&amp;display=swap" rel="stylesheet"> <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"> <style>.aaon {display: flex;} body {height: fit-content;margin: 0;padding: 0;}</style></head><body>`
 for (let i = 0; i < canvases.length; i++) {
-    exportHtml +=  `<!DOCTYPE html>
-    <html>
-    
-    <head>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
-      <link
-        href="https://fonts.googleapis.com/css2?family=Alegreya&amp;family=Poppins:ital,wght@0,500;1,600&amp;display=swap"
-        rel="stylesheet">
-    
-      <style>
-        .aaon {
-          display: flex;
-        }
-    
-        body {
-          height: fit-content;
-          margin: 0;
-          padding: 0;
-        }
-      </style>
-    </head>
-    
-    <body><div class='aaon'>`+canvases[i].toSVG()+`</div>
-    </body>
-    </html>`
+    exportHtml +=  `<div class='aaon'>`+canvases[i].toSVG()+`</div>`
 }
-
+exportHtml += `</body></html>`
 return exportHtml
 }
 
