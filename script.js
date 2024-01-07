7136609480c91bafddebb20b5c8b61260854d07a
45akshat <98400519+45akshat@users.noreply.github.com>
2024-01-07 18:20:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -399,7 +399,7 @@ AddPageVisibility()
 function createHtmlFromJson() {
     var exportHtml = `<!DOCTYPE html><html> <head> <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""> <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"> <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">  <style>.aaon {display: flex;} body {height: fit-content;margin: 0;padding: 0;}</style></head><body>`
 for (let i = 0; i < canvases.length; i++) {
-    exportHtml +=  `<div class='aaon'>`+canvases[i].toSVG({ width: 1542.6,height: 867.712})+`</div>`
+    exportHtml +=  `<div class='aaon'>`+canvases[i].toSVG({ width: 771.3,height: 433.856})+`</div>`
 }
 exportHtml += `</body></html>`
 return exportHtml
